06746890c54aaf4f5f0a8c94f792d1df59474bf9
Tao Wei <taowei@buffalo.edu>
2016-11-16 22:26:37 +0800

    PhD 2014

--- js/html_actuator4.js
+++ js/html_actuator4.js
@@ -3,7 +3,6 @@ function HTMLActuator() {
   this.scoreContainer   = document.querySelector(".score-container");
   this.bestContainer    = document.querySelector(".best-container");
   this.messageContainer = document.querySelector(".game-message");
-  this.sharingContainer = document.querySelector(".score-sharing");
 
   this.score = 0;
 }
@@ -27,9 +26,9 @@ HTMLActuator.prototype.actuate = function (grid, metadata) {
 
     if (metadata.terminated) {
       if (metadata.over) {
-        self.message(false); // You lose
+        self.message(false); // The game has ended 
       } else if (metadata.won) {
-        self.message(true); // You win!
+        self.message(true); // You can continue playing
       }
     }
 
@@ -38,10 +37,6 @@ HTMLActuator.prototype.actuate = function (grid, metadata) {
 
 // Continues the game (both restart and keep playing)
 HTMLActuator.prototype.continueGame = function () {
-  if (typeof ga !== "undefined") {
-    ga("send", "event", "game", "restart");
-  }
-
   this.clearMessage();
 };
 
@@ -51,6 +46,35 @@ HTMLActuator.prototype.clearContainer = function (container) {
   }
 };
 
+var captions = ["Coffee", "Panini",
+  "想法", "代码", "<span style='display:inline-block;line-height:1.2;vertical-align:middle'>深度<br>学習</span>", "见导师",
+  "实验", "Paper", "会议", "答辩", "PhD",
+  "薄厚", "僵尸", "Reader", "叫兽"];
+var game_title = "磗士";
+var caption_garbage = "垃圾";
+var captions_rel = ["恋爱", "分手"];
+
+var val2caption = function(val){
+  if(val <= 0) return caption_garbage;
+  if(val == 1){
+    var caption = "<span style='display:inline-block;line-height:1.2;vertical-align:middle'><span class='rel'>";
+    if(window.game.relTime) caption += captions_rel[0];
+    else caption += captions_rel[1];
+    caption += "</span><br><span class='karma'>" + window.game.karma + "</span></span>";
+    return caption;
+  }
+  var idx = -1;
+  var n = 1;
+  while(n < val) {
+    n <<= 1;
+    idx++;
+  }
+  if(idx >= 0 && idx < captions.length)
+    return captions[idx];
+  else
+    return val;
+};
+
 HTMLActuator.prototype.addTile = function (tile) {
   var self = this;
 
@@ -58,7 +82,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   var inner     = document.createElement("div");
   var position  = tile.previousPosition || { x: tile.x, y: tile.y };
   var positionClass = this.positionClass(position);
-
+  
   // We can't use classlist because it somehow glitches when replacing classes
   var classes = ["tile", "tile-" + tile.value, positionClass];
 
@@ -67,7 +91,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
-  inner.textContent = tile.value;
+  inner.innerHTML = val2caption(tile.value);
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
@@ -119,7 +143,7 @@ HTMLActuator.prototype.updateScore = function (score) {
   if (difference > 0) {
     var addition = document.createElement("div");
     addition.classList.add("score-addition");
-    addition.textContent = "+" + difference;
+    addition.textContent = "-" + difference;
 
     this.scoreContainer.appendChild(addition);
   }
@@ -129,20 +153,33 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
   this.bestContainer.textContent = bestScore;
 };
 
-HTMLActuator.prototype.message = function (won) {
-  var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
-
-  if (typeof ga !== "undefined") {
-    ga("send", "event", "game", "end", type, this.score);
+HTMLActuator.prototype.message = function (ended) {
+  var type    = ended ? "game-won" : "game-over";
+  var message = window.game.won ? "You got a PhD!" : "You got a <abbr title='Certificate of Postgraduate Studies'>CPGS</abbr>!";
+  if(!window.game.won) {
+    if(window.game.maxTile >= 1024) message = "One step away!";
+    else if(window.game.maxTile >= 512) message = "Not bad!";
+  }
+  else if(window.game.maxTile > 2048) {
+    message = "You got a " + val2caption(window.game.maxTile) + "!";
   }
 
   this.messageContainer.classList.add(type);
-  this.messageContainer.getElementsByTagName("p")[0].textContent = message;
+  this.messageContainer.getElementsByTagName("p")[0].innerHTML = message;
 
-  this.clearContainer(this.sharingContainer);
-  this.sharingContainer.appendChild(this.scoreTweetButton());
-  twttr.widgets.load();
+  ga('send', 'event', 'game', 'ended-' + window.game.maxTile, this.score);
+
+  var shareButtons = document.getElementsByClassName('share-fb');
+  for(var i=0; i<shareButtons.length; i++) shareButtons[i].style.display = "none";
+
+  if(!window.game.won) {
+    if(window.game.maxTile >= 1024) document.getElementById('share-1024').style.display = "";
+    else if(window.game.maxTile >= 512) document.getElementById('share-512').style.display = "";
+    else document.getElementById('share-cpgs').style.display = "";
+  }
+  else {
+    document.getElementById('share-phd').style.display = "";
+  }
 };
 
 HTMLActuator.prototype.clearMessage = function () {
@@ -151,18 +188,24 @@ HTMLActuator.prototype.clearMessage = function () {
   this.messageContainer.classList.remove("game-over");
 };
 
-HTMLActuator.prototype.scoreTweetButton = function () {
-  var tweet = document.createElement("a");
-  tweet.classList.add("twitter-share-button");
-  tweet.setAttribute("href", "https://twitter.com/share");
-  tweet.setAttribute("data-via", "gabrielecirulli");
-  tweet.setAttribute("data-url", "http://git.io/2048");
-  tweet.setAttribute("data-counturl", "http://gabrielecirulli.github.io/2048/");
-  tweet.textContent = "Tweet";
-
-  var text = "I scored " + this.score + " points at 2048, a game where you " +
-             "join numbers to score high! #2048game";
-  tweet.setAttribute("data-text", text);
-
-  return tweet;
+HTMLActuator.prototype.refreshRel = function (remainingTime) {
+  if(remainingTime > 0){
+    document.getElementById('title').textContent = "Love";
+    document.getElementsByClassName('game-intro')[0].textContent = "Your relationship will last for "+remainingTime+"s.";
+    document.getElementById('progress').textContent = "Love";
+    document.getElementById('progress').style.display = "";
+    if(window.innerWidth < 520)
+      document.getElementById('progress').style.width = Math.round(100*remainingTime/window.game.relDuration) + "px";
+    else
+      document.getElementById('progress').style.width = Math.round(200*remainingTime/window.game.relDuration) + "px";
+  }
+  else{
+    document.getElementById('title').textContent = game_title;
+    document.getElementsByClassName('game-intro')[0].textContent = "Move the bricks to complete your PhD.";
+    document.getElementById('progress').textContent = "";
+    document.getElementById('progress').style.display = "none";
+    document.getElementById('progress').style.width = "0";
+    var rels = document.getElementsByClassName('rel');
+    for(var i=0; i<rels.length; i++) rels[i].innerHTML = captions_rel[1];
+  }
 };
